06cf7edb12ec2ea4a39a345f335bd30eee9ea0ca
alfatta <arwinnersyah@gmail.com>
2025-03-07 09:40:44 +0700

    feat(seo): add web icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,98 @@ export default defineNuxtConfig({
         lang: "en",
       },
       title: "Alfatta Rezqa",
+      link: [
+        {
+          rel: "apple-touch-icon-precomposed",
+          href: "/img/apple-touch-icon-57x57.png",
+          sizes: "57x57",
+        },
+        {
+          rel: "apple-touch-icon-precomposed",
+          href: "/img/apple-touch-icon-60x60.png",
+          sizes: "60x60",
+        },
+        {
+          rel: "apple-touch-icon-precomposed",
+          href: "/img/apple-touch-icon-72x72.png",
+          sizes: "72x72",
+        },
+        {
+          rel: "apple-touch-icon-precomposed",
+          href: "/img/apple-touch-icon-76x76.png",
+          sizes: "76x76",
+        },
+        {
+          rel: "apple-touch-icon-precomposed",
+          href: "/img/apple-touch-icon-114x114.png",
+          sizes: "114x114",
+        },
+        {
+          rel: "apple-touch-icon-precomposed",
+          href: "/img/apple-touch-icon-120x120.png",
+          sizes: "120x120",
+        },
+        {
+          rel: "apple-touch-icon-precomposed",
+          href: "/img/apple-touch-icon-144x144.png",
+          sizes: "144x144",
+        },
+        {
+          rel: "apple-touch-icon-precomposed",
+          href: "/img/apple-touch-icon-152x152.png",
+          sizes: "152x152",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/img/favicon-196x196.png",
+          sizes: "196x196",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/img/favicon-96x96.png",
+          sizes: "96x96",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/img/favicon-32x32.png",
+          sizes: "32x32",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/img/favicon-16x16.png",
+          sizes: "16x16",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/img/favicon-128.png",
+          sizes: "128",
+        },
+      ],
       meta: [
+        { name: "application-name", content: "Alfatta Rezqa" },
+        { name: "msapplication-TileColor", content: "#FFFFFF" },
+        { name: "msapplication-TileImage", content: "/img/mstile-144x144.png" },
+        {
+          name: "msapplication-square70x70logo",
+          content: "/img/mstile-70x70.png",
+        },
+        {
+          name: "msapplication-square150x150logo",
+          content: "/img/mstile-150x150.png",
+        },
+        {
+          name: "msapplication-wide310x150logo",
+          content: "/img/mstile-310x150.png",
+        },
+        {
+          name: "msapplication-square310x310logo",
+          content: "/img/mstile-310x310.png",
+        },
         {
           name: "description",
           content:
