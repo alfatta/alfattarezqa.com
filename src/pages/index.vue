dfc7bc6de0ee332e8fb60c7169ec4fef59a94034
alfatta <arwinnersyah@gmail.com>
2025-05-30 11:25:09 +0700

    feat(project): show projects

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -51,6 +51,23 @@ defineOgImageComponent('Base', {
       </ul>
     </Section>
 
+    <Section title="Projects">
+      <div class="grid md:grid-cols-2 gap-4">
+        <div class="text-slate-500 rounded-xl border border-slate-200 px-4 py-4 hover:bg-slate-50 duration-300 transition-all">
+          <div class="space-y-2">
+            <div class="flex items-center gap-2">
+              <h3 class="font-medium text-slate-900">PixRelay</h3>
+              <div class="flex gap-2">
+                <a href="https://hub.docker.com/r/alfatta/pixrelay" target="_blank" class="rounded-md bg-slate-100 hover:bg-slate-200 px-2 py-1 text-xs duration-300 transition-all">Docker Hub</a>
+                <a href="https://github.com/alfatta/pixrelay" target="_blank" class="rounded-md bg-slate-100 hover:bg-slate-200 px-2 py-1 text-xs duration-300 transition-all">GitHub</a>
+              </div>
+            </div>
+            <p class="text-sm line-clamp-2">A fast and efficient image processing service that allows you to resize images from any URL without manually downloading and uploading them.</p>
+          </div>
+        </div>
+      </div>
+    </Section>
+
     <Section title="Latest Blog">
       <template #action>
         <NuxtLink to="/blog/" class="text-xs text-slate-500 hover:text-slate-600">
